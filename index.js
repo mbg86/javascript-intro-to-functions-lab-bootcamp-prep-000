4c45e4120593b3254a65d97673cc8ddc8ff2d320
Manrique Buso - Garcia <manrique.buso@gmail.com>
2018-10-22 00:40:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string.toLowerCase())
-  return "I can't hear you!";
+  return "I can't hear you!"
 }
 
 function sayHiToGrandma(string){
   if (string === string.toUpperCase())
-  return "YES INDEED!";
+  return "YES INDEED!"
 }
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase())
-  return "I love you too!";
+  return "I love you too!"
 }
